837a78505edb77f229fda35e18ce3cbf91ddc749
Nabeel Ali <nabeelali_1207@outlook.com>
2024-05-09 09:58:32 +0100

    added currentDir print

--- Cpp/HelloWorld/HelloWorld.cpp
+++ Cpp/HelloWorld/HelloWorld.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <filesystem>
 int main()
 {
-    std::cout << "Hello World!" << std::endl;
-    return 0;
+	std::cout << "Hello World!" << std::endl;
+	std::filesystem::path currentDir = std::filesystem::current_path();
+	std::cout << "Current directory: " << currentDir << std::endl;
+	char temp;
+	std::cin >> temp;
+	return 0;
 }
